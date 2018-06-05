ab2300a26a5fa658ea7f085f5d44dbb41a3091e9
HexRdesign <support@hexr.org>
2018-06-04 22:31:55 -0500

    :sparkles: Add support for refresh intervals
    Now you can trigger your index to rebuild every so often!

--- server.js
+++ server.js
@@ -2,16 +2,35 @@ const http = require('http');
 const GhostHunter = require('./lib/ghost-hunter');
 const serveResponse = require('./lib/response');
 
+function errorAndExit(error) {
+    console.error(error);
+    console.log('An unrecoverable error has occurred');
+    process.exit(1);
+}
+
 module.exports = function startServer(options) {
     this.options = Object.assign({
         port: 3000,
         path: '/',
-        ghostHunter: {}
+        ghostHunter: {},
+        refreshInterval: 1000 * 60 * 60 * 6 // Refresh the index every 6 hours
     }, options);
     this.indexer = new GhostHunter(options.ghostHunter);
+    console.log('[Index] Creating initial index');
     this.indexer.createIndex()
-        .then(() => console.log('server ready'))
-        .catch(e => console.error(e));
+        .then(() => console.log('[Index] Initial population done - server is ready'))
+        .catch(errorAndExit);
+
+    if (this.options.refreshInterval) {
+        this.refreshID = setInterval(() => {
+            console.log('[Index] Refreshing');
+            this.indexer.createIndex()
+                .then(() => console.log('[Index] refreshed'))
+                .catch(errorAndExit);
+        }, this.options.refreshInterval);
+    } else {
+        this.refreshID = null;
+    }
 
     this.server = http.createServer(serveResponse.bind(this));
     this.server.listen({
