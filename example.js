77064c5584431b37d8e46222a7ece6e03db67be5
HexRdesign <support@hexr.org>
2018-06-27 10:43:52 -0500

    :rotating_light: spaces->tabs

--- example.js
+++ example.js
@@ -2,13 +2,13 @@ const Server = require('./server');
 
 // Basic server example!
 const server = new Server({
-    path: '/',
-    ghostHunter: {
-        // Update your config here!
-        instance: {
-            url: 'https://yoursite.com/blog/',
-            client_id: 'ghost-search',
-            client_secret: 'c57de3xq3c2h'
-        }
-    }
+	path: '/',
+	ghostHunter: {
+		// Update your config here!
+		instance: {
+			url: 'https://yoursite.com/blog/',
+			client_id: 'ghost-search',
+			client_secret: 'c57de3xq3c2h'
+		}
+	}
 });
--- server.js
+++ server.js
@@ -3,42 +3,42 @@ const GhostHunter = require('./lib/ghost-hunter');
 const serveResponse = require('./lib/response');
 
 function errorAndExit(error) {
-    console.error(error);
-    console.log('An unrecoverable error has occurred');
-    process.exit(1);
+	console.error(error);
+	console.log('An unrecoverable error has occurred');
+	process.exit(1);
 }
 
 module.exports = function startServer(options) {
-    this.options = Object.assign({
-        port: 3000,
-        path: '/',
-        ghostHunter: {},
-        refreshInterval: 1000 * 60 * 60 * 6, // Refresh the index every 6 hours
-        allowedOrigins: []
-    }, options);
-    this.indexer = new GhostHunter(options.ghostHunter);
-    console.log('[Index] Creating initial index');
-    this.indexer.createIndex()
-        .then(() => console.log('[Index] Initial population done - server is ready'))
-        .catch(errorAndExit);
+	this.options = Object.assign({
+		port: 3000,
+		path: '/',
+		ghostHunter: {},
+		refreshInterval: 1000 * 60 * 60 * 6, // Refresh the index every 6 hours
+		allowedOrigins: []
+	}, options);
+	this.indexer = new GhostHunter(options.ghostHunter);
+	console.log('[Index] Creating initial index');
+	this.indexer.createIndex()
+		.then(() => console.log('[Index] Initial population done - server is ready'))
+		.catch(errorAndExit);
 
-    if (this.options.refreshInterval) {
-        this.refreshID = setInterval(() => {
-            console.log('[Index] Refreshing');
-            this.indexer.createIndex(true)
-                .then(() => console.log('[Index] refreshed'))
-                // @todo: fallback to old index???
-                .catch(errorAndExit);
-        }, this.options.refreshInterval);
-    } else {
-        this.refreshID = null;
-    }
+	if (this.options.refreshInterval) {
+		this.refreshID = setInterval(() => {
+			console.log('[Index] Refreshing');
+			this.indexer.createIndex(true)
+				.then(() => console.log('[Index] refreshed'))
+				// @todo: fallback to old index???
+				.catch(errorAndExit);
+		}, this.options.refreshInterval);
+	} else {
+		this.refreshID = null;
+	}
 
-    this.server = http.createServer(serveResponse.bind(this));
-    this.server.listen({
-        host: this.options.host,
-        port: this.options.port
-    });
+	this.server = http.createServer(serveResponse.bind(this));
+	this.server.listen({
+		host: this.options.host,
+		port: this.options.port
+	});
 
-    return this;
+	return this;
 };
